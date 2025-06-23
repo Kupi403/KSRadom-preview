42932bcdda5928380120bb449d02e76ce5989d6e
Michał Kupidura <87815922+Kupi403@users.noreply.github.com>
2025-06-23 15:17:23 +0200

    update url.ts

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API  || 'http://localhost:1337/api'
-export const URL = process.env.NEXT_PUBLIC_BACKEND || 'http://localhost:1337'
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API || 'http://93.127.186.161:1337/api'
+export const URL = process.env.NEXT_PUBLIC_BACKEND || 'http://93.127.186.161:1337'
